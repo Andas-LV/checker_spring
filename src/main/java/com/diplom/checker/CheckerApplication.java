58df820ff74d6c9b18de56624adaf3540bfbd501
Andas-LV <andassatybaldy@gmail.com>
2025-05-19 19:29:35 +0500

    feat: swagger changes

--- src/main/java/com/diplom/checker/CheckerApplication.java
+++ src/main/java/com/diplom/checker/CheckerApplication.java
@@ -3,6 +3,16 @@ package com.diplom.checker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+  servers = {
+    // относительный URL: Swagger UI будет брать текущий хост и протокол
+    @Server(url = "/", description = "Default Server URL")
+  }
+)
+
 @SpringBootApplication
 public class CheckerApplication {
 	public static void main(String[] args) {
